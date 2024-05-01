91142ab6102c784e0961b90bf185bf986a126dac
Poojan-98 <chapagain.pujan@gmail.com>
2024-05-01 08:13:16 +0545

    style: 404

--- src/views/404View.vue
+++ src/views/404View.vue
@@ -1,16 +1,8 @@
 <script setup></script>
 <template>
-  <main
-    class="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8"
-  >
+  <main class="">
     <div class="">
-      <router-link to="https://poojanchapagain.com.np/" class="text-center"
-        ><img
-          src="/images/mylogo.png"
-          class="mx-auto max-w-[150px]"
-          alt="pujan chapagain logo"
-      /></router-link>
-      <div class="mx-auto mt-10 max-w-2xl text-center md:mt-16">
+      <div class="mx-auto max-w-2xl text-center md:mt-16">
         <p class="text-base font-semibold leading-8 text-indigo-600">404</p>
         <h1
           class="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl"
@@ -24,11 +16,19 @@
         </p>
         <router-link
           to="/"
-          class="text-sm inline-block font-semibold leading-6 text-indigo-600 pt-10"
+          class="inline-block font-semibold leading-6 text-indigo-600 hover:text-indigo-800 pt-10"
         >
           <span aria-hidden="true">&larr;</span>
           Back to home
         </router-link>
+        <div class="mt-10">
+          <router-link to="https://poojanchapagain.com.np/" class="text-center"
+            ><img
+              src="/images/mylogo.png"
+              class="mx-auto max-w-[150px]"
+              alt="pujan chapagain logo"
+          /></router-link>
+        </div>
       </div>
     </div>
   </main>
