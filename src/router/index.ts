f74a48ec6caf08687c73d366f015050b14dcbb4c
Poojan-98 <chapagain.pujan@gmail.com>
2024-05-01 07:58:59 +0545

    fix(netlify):fixed reload blank issue, page not found issue

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,37 +1,43 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import LayoutSelector from '../views/Layout.vue'
-import ListView from '../views/ListView.vue'
-import Choice from '../views/QuestionSelection.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import LayoutSelector from "../views/Layout.vue";
+import ListView from "../views/ListView.vue";
+import ErrorView from "../views/404View.vue";
+import Choice from "../views/QuestionSelection.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'layout',
-      component: LayoutSelector
+      path: "/",
+      name: "layout",
+      component: LayoutSelector,
     },
     {
-      path: '/choice',
-      name: 'choice',
-      component: Choice
+      path: "/choice",
+      name: "choice",
+      component: Choice,
     },
     {
-      path: '/',
-      name: 'layout',
-      component: LayoutSelector
+      path: "/",
+      name: "layout",
+      component: LayoutSelector,
     },
     {
-      path: '/list',
-      name: 'list',
-      component: ListView
+      path: "/list",
+      name: "list",
+      component: ListView,
     },
     {
-      path: '/pagination',
-      name: 'pagination',
-      component: () => import('../views/PaginationView.vue')
-    }
-  ]
-})
+      path: "/pagination",
+      name: "pagination",
+      component: () => import("../views/PaginationView.vue"),
+    },
+    {
+      path: "/:pathMatch(.*)",
+      name: "404",
+      component: ErrorView,
+    },
+  ],
+});
 
-export default router
+export default router;
